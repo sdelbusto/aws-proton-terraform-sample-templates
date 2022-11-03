e0f466b414ebd665eb9209587ceb140d899008e4
sbx_user1051 <sbx_user1051@169.254.54.117>
2022-11-03 16:36:24 +0000

    Update requested for Proton - environment/fargate-env

---
+++ fargate-env/locals.tf
@@ -0,0 +1,9 @@
+/*
+This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
+*/
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.id
+  partition  = data.aws_partition.current.id
+}
